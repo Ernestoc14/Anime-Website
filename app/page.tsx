1710a7dfd45a1c90ae6c9be8f718011384125e9f
Ernestoc14 <descreed34@hotmail.com>
2024-05-01 21:13:29 -0500

    Feat: Add a AnimeCard Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
 import { data } from "./_data";
+import AnimeCard from "@/components/AnimeCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
       <div className="grid grid-cols-2 gap-4">
-        {data.map((anime) => (
-          <div key={anime.id}>
-            <p className="font-bold mb-4">{anime.name}</p>
-            <Image
-              src={anime.image.original}
-              alt="anime.name"
-              width={150}
-              height={5}
-            />
-          </div>
+        {data.map((anime, item) => (
+          <AnimeCard key={anime.id} anime={data[item]} index={0} />
         ))}
       </div>
     </main>

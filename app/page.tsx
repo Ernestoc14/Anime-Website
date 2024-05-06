c11a0018d2e10dddcdd29f289f29c7e29e885c07
Ernestoc14 <descreed34@hotmail.com>
2024-05-06 00:27:39 -0500

    Fix: Changing Dollar Sign in the Fetch URL (Server Action)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { data } from "./_data";
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+import { fetchAnime } from "./action";
 
-export default function Home() {
+export default async function Home() {
+  await fetchAnime(1)
+
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
       <div className="grid grid-cols-2 gap-4">

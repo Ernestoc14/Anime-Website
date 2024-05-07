741be05493250815dcf6fbcd5f13a3fae142cceb
Ernestoc14 <descreed34@hotmail.com>
2024-05-07 11:39:14 -0500

    Feat: Adding Infinite Scroll to render more anime pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 // import { data } from "./_data";
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 import { fetchAnime } from "./action";
+import LoadMore from "@/components/LoadMore";
 
 export default async function Home() {
-  const data = await fetchAnime(1)
+  const data = await fetchAnime(1);
 
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <div className="grid grid-cols-2 gap-4">
+    <main className="flex flex-col min-h-screen items-center justify-center sm:p-16 py-16 px-8 gap-10">
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {data.map((item: AnimeProp, index: number) => (
           <AnimeCard key={item.id} anime={item} index={index} />
         ))}
-      </div>
+      </section>
+      <LoadMore />
     </main>
   );
 }
---
+++ components/LoadMore.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { fetchAnime } from "@/app/action";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { InView, useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
+type Props = {};
+
+function LoadMore({}: Props) {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
+    }
+  }, [inView, data]);
+
+  return (
+    <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <div ref={ref}>
+        <Image src="/spinner.svg" alt="loading" width={50} height={50} />
+      </div>
+    </>
+  );
+}
+
+export default LoadMore;
